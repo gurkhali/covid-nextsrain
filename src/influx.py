193d1680d94b5448f72b3edb504c1202003f0c92
gurkhali <346298+gurkhali@users.noreply.github.com>
2020-05-19 19:52:42 +0000

    Remove API key

--- src/influx.py
+++ src/influx.py
@@ -4,11 +4,6 @@ from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-#API_KEY = "QcNRDI-5FB45PTPo431Lhqz3Zo0P2u4Du0Uye1yVRdTFFkXNWjfgbm-rMizJc_Pw0KCfpH7Df5_SOcAH7-UX0g=="
-#host = "https://us-west-2-1.aws.cloud2.influxdata.com"
-#org = "0df6a528218ccee0"
-#client = InfluxDBClient(url="https://us-west-2-1.aws.cloud2.influxdata.com", token=API_KEY)
-
 API_KEY = f = open('../creds/influx.token').read()[:-1]
 bucket = "test"
 org = "05a92510f9f10000"
